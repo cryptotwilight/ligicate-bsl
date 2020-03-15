642bc9916cd78acb3a89ad64159e7f686c14c44f
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2020-03-15 16:31:20 +0000

    lender initial version

---
+++ lender/src/main/java/com/pawnmystuff/algo/Lender.java
@@ -0,0 +1,12 @@
+package com.pawnmystuff.algo;
+
+public class Lender {
+
+	public static void main(String[] args) {
+		// create account 
+		// request credit 
+		
+
+	}
+
+}
---
+++ lender/src/main/java/com/pawnmystuff/algo/lender/LoanDashboard.java
@@ -0,0 +1,27 @@
+package com.pawnmystuff.algo.lender;
+
+import java.util.List;
+import com.pawnmystuff.algo.model.Fund;
+import com.pawnmystuff.algo.model.Loan;
+import com.pawnmystuff.algo.model.LoanApplication;
+import com.pawnmystuff.algo.model.Offer;
+
+public class LoanDashboard {
+
+	
+	public List<Loan> getLoans() { 
+		return null; 
+	}
+	
+	public List<Offer> getOffers() { 
+		return null; 
+	}
+	
+	public List<LoanApplication> getLoanApplications() {
+		return null; 
+	}
+	
+	public Fund getFunds() { 
+		return null; 
+	}
+}
